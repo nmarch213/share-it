8bc358f17091d8d781df601501abb0b005af9da5
Nicholas March <n.march213@gmail.com>
2019-03-11 00:22:11 -0500

    updated header for some styling

--- components/header/header.js
+++ components/header/header.js
@@ -2,17 +2,48 @@ import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const HeaderStyle = styled.div`
-  font-size: 30px;
+const Wrapper = styled.div`
+  font-size: 2rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Nav = styled.a`
+  font-weight: 400;
+  color: rgba(255, 255, 255, 0);
+  display: inline-block;
+  cursor: pointer;
+  text-decoration: none;
+  padding: 0.6rem 1.6rem;
+  margin: 0.6rem;
+  border-style: solid;
+  border-width: 1px;
+  transition: all 0.2s ease;
+  border-image: ${props => props.theme.borderGradient};
+  background: ${props => props.theme.bgGradient};
+  background-clip: text;
+  -webkit-background-clip: text;
+`;
+
+const Brand = styled.div`
+  font-size: 3rem;
 `;
 
 const Header = () => (
-  <HeaderStyle>
-    Share It
-    <Link href="/about">
-      <a>About Page</a>
-    </Link>
-  </HeaderStyle>
+  <Wrapper>
+    <Brand>Share It</Brand>
+    <div>
+      <Link href="/about">
+        <Nav>About</Nav>
+      </Link>
+      <Link href="/sign-up">
+        <Nav>Sign up</Nav>
+      </Link>
+      <Link href="/log-in">
+        <Nav>Log In</Nav>
+      </Link>
+    </div>
+  </Wrapper>
 );
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import Header from '../components/header/header';
+import GlobalStyle from '../styles/global';
+import theme from '../styles/theme';
 
-const Index = () => (
-  <div>
-    <Header />
-    <p>Hello Next</p>
-  </div>
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <div>
+      <GlobalStyle />
+      <Header className="" />
+      <p>Hello Next</p>
+    </div>
+  </ThemeProvider>
 );
 
-export default Index;
+export default App;
