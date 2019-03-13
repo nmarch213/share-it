8f34f4e6825b80370d2604b6bccb56d6eba0c401
Nicholas March <n.march213@gmail.com>
2019-03-12 22:16:51 -0500

    added prettier rc updated eslint for always parens to fight airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,8 @@
-module.exports = { "extends": "airbnb-base" };
+module.exports = {
+  extends: 'airbnb-base',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'arrow-parens': 'always',
+  },
+};
